21e5c34b513855a4006e2b6849a4d7d9f4e8880d
Jar7011 <jareyes1324@gmail.com>
2024-11-21 20:52:21 -0700

    Finished ARIMA models on two store-item combinations

--- Item_Demand_ARIMA.R
+++ Item_Demand_ARIMA.R
@@ -3,6 +3,7 @@ library(timetk)
 library(tidyverse)
 library(tidymodels)
 library(vroom)
+library(plotly)
 
 # Read in the data
 train_data <- vroom('train.csv')
@@ -20,8 +21,63 @@ test_combination_2 <- test_data %>%
   filter(store == 5, item == 10)
 
 
-# Create the CV split
+# Create the CV split and graphs
 cv_split_1 <- time_series_split(train_combination_1, assess="3 months", cumulative = TRUE)
+cv_plot_1 <- cv_split_1 %>%
+  tk_time_series_cv_plan() %>%
+  plot_time_series_cv_plan(date, sales, .interactive=FALSE)
+
 cv_split_2 <- time_series_split(train_combination_2, assess="3 months", cumulative = TRUE)
+cv_plot_2 <- cv_split_2 %>%
+  tk_time_series_cv_plan() %>%
+  plot_time_series_cv_plan(date, sales, .interactive=FALSE)
 
 # Create ARIMA recipe
+recipe_1 <- recipe(sales ~ ., data = train_combination_1) %>% 
+  step_rm(c('store', 'item'))
+recipe_2 <- recipe(sales ~ ., data = train_combination_2) %>% 
+  step_rm(c('store', 'item'))
+
+# Define ARIMA models
+arima_model <- arima_reg() %>%
+  set_engine("auto_arima")
+
+# Create workflow
+arima_wf_1 <- workflow() %>% 
+  add_recipe(recipe_1) %>% 
+  add_model(arima_model) %>% 
+  fit(data = training(cv_split_1))
+arima_wf_2 <- workflow() %>% 
+  add_recipe(recipe_2) %>% 
+  add_model(arima_model) %>% 
+  fit(data = training(cv_split_2))
+
+# Tune the models
+cv_results_1 <- modeltime_calibrate(arima_wf_1, new_data = testing(cv_split_1))
+cv_results_2 <- modeltime_calibrate(arima_wf_2, new_data = testing(cv_split_2))
+
+# Visualize results
+cv_results_1_plot <- cv_results_1 %>% 
+  modeltime_forecast(new_data = testing(cv_split_1),
+                     actual_data = training(cv_split_1)) %>% 
+  plot_modeltime_forecast(.interactive = FALSE)
+
+cv_results_2_plot <- cv_results_2 %>% 
+  modeltime_forecast(new_data = testing(cv_split_2),
+                     actual_data = training(cv_split_2)) %>% 
+  plot_modeltime_forecast(.interactive = FALSE)
+
+# Refit to whole dataset
+fullfit_1 <- cv_results_1 %>% 
+  modeltime_forecast(new_data = test_combination_1,
+                     actual_data = train_combination_1) %>% 
+  plot_modeltime_forecast(.interactive = FALSE)
+
+fullfit_2 <- cv_results_2 %>% 
+  modeltime_forecast(new_data = test_combination_2,
+                     actual_data = train_combination_2) %>% 
+  plot_modeltime_forecast(.interactive = FALSE)
+
+# Create a 4-panel plot
+plot <- subplot(cv_results_1_plot, cv_results_2_plot,
+                fullfit_1, fullfit_2, nrows = 2)
